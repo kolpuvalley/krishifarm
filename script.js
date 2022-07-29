65e12793dd2142434efe6facb105ae8cd96754ba
kolpuvalley <110207383+kolpuvalley@users.noreply.github.com>
2022-07-29 06:09:16 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function english(){
+       document.getElementById('topic').innerHTML="Kolpu Valley Krishi Farm";
+       document.getElementById('home').innerHTML="Home"; 
+       document.getElementById('about').innerHTML="About Us"; 
+       document.getElementById('who').innerHTML="Who are we?"; 
+        document.getElementById('facebook').innerHTML="Connect on Facebook";
+        document.getElementById('gallery').innerHTML="Gallery";
+       document.getElementById('p1').innerHTML="Kolpu Valley Krishi Farm is an agricultural startup initiated by Manoj Mudvari in 2018. Being a multipurpose organization, our vision is production, processing and marketing of agriculture.";
+       document.getElementById('p2').innerHTML="We are located on Kumari, Belkotgadhi Muncipility-03 of Nuwakot District. We have a wide range of activities from Fruits/Vegetable production to livestock rearing. We try to promote agriculture as a secure profession for youths and make them self sustained in their homeland. Our target is to contribute to the economy of the nation through a revolution in agriculture.";
+       document.getElementById('p3').innerHTML="Our target is to provide products to our customers at a very affordable rate by minimizing the role of middle-man. We try to minimize the gap between agricultural producers and end-user. This would lead to a substantial decrease in price for customers and ensure welfare for farmers.";
+       document.getElementById('desc').innerHTML="Manoj Mudvari is an enthusiastic entrepreneur. Having completed an MBA, he has experience in various fields on his entrepreneurship journey. Since 2018, he has started Kolpu Valley Krishi Farm with the unique concept.";
+            }
+
+
+function nepali() {
+      document.getElementById('topic').innerHTML="कोल्पूभ्याली कृषि फर्म";
+       document.getElementById('home').innerHTML="गृहपृष्ठ"; 
+       document.getElementById('about').innerHTML="हाम्रो बारेमा"; 
+       document.getElementById('who').innerHTML="हामी को हौ?";
+       document.getElementById('facebook').innerHTML="फेसबुकमा जोडिनुहोस";
+       document.getElementById('gallery').innerHTML="तस्बिरहरु ";
+      document.getElementById('p1').innerHTML="कोल्पूभ्याली कृषि फर्म एक कृषि व्यवसाय हो जसको सुरुवात मनोज मुडभरीले २०७५ सालमा गरेका हुन। एक बहुआयमिक र बहूउद्देशिय संस्था भएकाले हामी आफ्नो कृषि उत्पादनको साथै त्यसको प्रसोधन र बजारीकरणमा समेत ध्यान दिन्छौ।";
+       document.getElementById('p2').innerHTML="नुवाकोट जिल्लाको बेल्कोटगढी नगरपालिका -०३ मा अवस्थित हाम्रो फर्ममा फलफुल/तरकारी खेति देखि लिएर पशुपालन सम्मको कार्य हरु गरिन्छ।  हामी कृषिलाइ एक उत्तम पेशाको रुपमा प्रचार गर्नु को साथै युवाहरुलाई आफ्नै देशमा केहि गर्न प्रेरित गर्छौ।  हाम्रो लक्ष्य भनेकै कृषिको माध्यमबाट यो रास्ट्रको अर्थतन्त्रमा क्रान्ति ल्याउनु हो।";
+       document.getElementById('p3').innerHTML="मध्यमार्गी लाई निरुत्साहित गरेर हामी हाम्रा ग्राहक लाई निकै नै सस्तो मुल्यमा आफ्नो उत्पादन बिक्रि गर्नेछौ। कृषक र ग्राहक बिचको दुरी कम गरी निकै स्वस्थ प्रतिस्पर्धाको माध्यम बाट हाम्रो व्यवसाय चल्दछ।";
+       document.getElementById('desc').innerHTML="मनोज मुडभरी एक व्यवसायी व्यक्तित्व हुन। एमबिए गरेका उनले धेरै व्यवसायिक क्षेत्र हरुमा अनुभव बटुलेका छन। सन् २०१८ देखि उनले नयाँ सोचका साथ कोल्पू भ्याली कृषि फर्म सुरु गरेका हुन।  ";
+      }
